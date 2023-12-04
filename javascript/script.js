723c52970b1dcc19be71f12c7242753167d3984b
jqayumov909 <jqayumov909@gmail.com>
2023-12-05 03:28:51 +0500

    feat: lrn react

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,10 @@
-//  #demo,  #web
+//ID - a,
+const arr = [2, 3, 78, 21, 34 ,9];
+document.getElementById("a").innerHTML = arr;
+
+function func1() {
+  document.getElementById("a").innerHTML = arr.sort();
+}
+function func2() {
+  document.getElementById("a").innerHTML = arr.sort((function(a, b){return b - a}));
+}
