c7d1d3c34780f6636983ef6c33f079da16fbfd7d
jqayumov909 <jqayumov909@gmail.com>
2023-12-05 20:40:41 +0500

    first-big-project-starting

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,1 @@
-//ID - a,
-const arr = [2, 3, 78, 21, 34 ,9];
-document.getElementById("a").innerHTML = arr;
-
-function func1() {
-  document.getElementById("a").innerHTML = arr.sort();
-}
-function func2() {
-  document.getElementById("a").innerHTML = arr.sort((function(a, b){return b - a}));
-}
+"use strict";
